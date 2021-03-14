d18d0c589a956a4ccb9f3c00c3b79d84af2d43fc
Timothy Yaukey <yaukey@willowsoft.com>
2021-03-13 21:39:36 -0800

    Made vendor product import use a listview instead of a multi-line text box.

--- UI/SetupForms/ImportProductsForm.Designer.cs
+++ UI/SetupForms/ImportProductsForm.Designer.cs
@@ -29,29 +29,22 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportProductsForm));
-            this.txtInput = new System.Windows.Forms.TextBox();
             this.lblVendor = new System.Windows.Forms.Label();
             this.btnCreate = new System.Windows.Forms.Button();
             this.lblExplain = new System.Windows.Forms.Label();
             this.btnEnter = new System.Windows.Forms.Button();
-            this.txtColumnHeaders = new System.Windows.Forms.TextBox();
+            this.lvwNewProducts = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colVendorCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colRetail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCsCost = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCsSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colEaCost = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colBrand = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSubCat = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colIsActive = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
-            // 
-            // txtInput
-            // 
-            this.txtInput.AcceptsTab = true;
-            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtInput.HideSelection = false;
-            this.txtInput.Location = new System.Drawing.Point(12, 118);
-            this.txtInput.MaxLength = 1000000;
-            this.txtInput.Multiline = true;
-            this.txtInput.Name = "txtInput";
-            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtInput.Size = new System.Drawing.Size(892, 360);
-            this.txtInput.TabIndex = 1;
-            this.txtInput.WordWrap = false;
             // 
             // lblVendor
             // 
@@ -93,47 +86,111 @@
             this.btnEnter.UseVisualStyleBackColor = true;
             this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
             // 
-            // txtColumnHeaders
+            // lvwNewProducts
             // 
-            this.txtColumnHeaders.AcceptsTab = true;
-            this.txtColumnHeaders.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtColumnHeaders.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.txtColumnHeaders.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtColumnHeaders.Location = new System.Drawing.Point(15, 92);
-            this.txtColumnHeaders.Multiline = true;
-            this.txtColumnHeaders.Name = "txtColumnHeaders";
-            this.txtColumnHeaders.Size = new System.Drawing.Size(892, 20);
-            this.txtColumnHeaders.TabIndex = 12;
-            this.txtColumnHeaders.Text = "(column headers)";
-            this.txtColumnHeaders.WordWrap = false;
+            this.lvwNewProducts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwNewProducts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colSize,
+            this.colVendorCode,
+            this.colRetail,
+            this.colCsCost,
+            this.colCsSize,
+            this.colEaCost,
+            this.colBrand,
+            this.colSubCat,
+            this.colIsActive});
+            this.lvwNewProducts.FullRowSelect = true;
+            this.lvwNewProducts.GridLines = true;
+            this.lvwNewProducts.Location = new System.Drawing.Point(15, 118);
+            this.lvwNewProducts.Name = "lvwNewProducts";
+            this.lvwNewProducts.Size = new System.Drawing.Size(889, 360);
+            this.lvwNewProducts.TabIndex = 13;
+            this.lvwNewProducts.UseCompatibleStateImageBehavior = false;
+            this.lvwNewProducts.View = System.Windows.Forms.View.Details;
+            // 
+            // colName
+            // 
+            this.colName.Text = "Name";
+            this.colName.Width = 200;
+            // 
+            // colSize
+            // 
+            this.colSize.Text = "Size";
+            // 
+            // colVendorCode
+            // 
+            this.colVendorCode.Text = "Vendor Code";
+            this.colVendorCode.Width = 100;
+            // 
+            // colRetail
+            // 
+            this.colRetail.Text = "Retail";
+            this.colRetail.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // colCsCost
+            // 
+            this.colCsCost.Text = "Case Cost";
+            this.colCsCost.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // colCsSize
+            // 
+            this.colCsSize.Text = "Case Size";
+            this.colCsSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // colEaCost
+            // 
+            this.colEaCost.Text = "Ea Cost";
+            this.colEaCost.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // colBrand
+            // 
+            this.colBrand.Text = "Brand";
+            this.colBrand.Width = 100;
+            // 
+            // colSubCat
+            // 
+            this.colSubCat.Text = "Subcategory";
+            this.colSubCat.Width = 100;
+            // 
+            // colIsActive
+            // 
+            this.colIsActive.Text = "Is Active";
             // 
             // ImportProductsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(916, 519);
-            this.Controls.Add(this.txtColumnHeaders);
+            this.Controls.Add(this.lvwNewProducts);
             this.Controls.Add(this.btnEnter);
             this.Controls.Add(this.lblExplain);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.lblVendor);
-            this.Controls.Add(this.txtInput);
             this.Name = "ImportProductsForm";
             this.Text = "Import Products";
-            this.Load += new System.EventHandler(this.ImportProductsForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtInput;
         private System.Windows.Forms.Label lblVendor;
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.Label lblExplain;
         private System.Windows.Forms.Button btnEnter;
-        private System.Windows.Forms.TextBox txtColumnHeaders;
+        private System.Windows.Forms.ListView lvwNewProducts;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.ColumnHeader colSize;
+        private System.Windows.Forms.ColumnHeader colVendorCode;
+        private System.Windows.Forms.ColumnHeader colRetail;
+        private System.Windows.Forms.ColumnHeader colCsCost;
+        private System.Windows.Forms.ColumnHeader colCsSize;
+        private System.Windows.Forms.ColumnHeader colEaCost;
+        private System.Windows.Forms.ColumnHeader colBrand;
+        private System.Windows.Forms.ColumnHeader colSubCat;
+        private System.Windows.Forms.ColumnHeader colIsActive;
     }
 }
--- UI/SetupForms/ImportProductsForm.cs
+++ UI/SetupForms/ImportProductsForm.cs
@@ -19,11 +19,8 @@ namespace Willowsoft.Ordering.UI.SetupForms
     {
         private Vendor mVendor;
         private List<ProductSubCategory> mAllSubcategories;
-
-        private const int EM_SETTABSTOPS = 0x00CB;
-
-        [DllImport("User32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr SendMessage(IntPtr h, int msg, int wParam, int[] lParam);
+        List<ProductBrand> mBrands;
+        List<NewProductData> mProductDataToSave;
 
         public ImportProductsForm()
         {
@@ -35,30 +32,65 @@ namespace Willowsoft.Ordering.UI.SetupForms
             mVendor = vendor;
             mAllSubcategories = allSubcategories;
             lblVendor.Text = "Vendor: " + vendor.VendorName;
+            if (ParseInput())
+                return;
             this.ShowDialog();
-        }
-
-        private void ImportProductsForm_Load(object sender, EventArgs e)
-        {
-            txtColumnHeaders.Text = "Name\tSize\tCode\tRetail\tCsCost\tCsSize\tEaCost\tBrand\tSubcategory\tActive";
-            SetTabWidth(txtColumnHeaders);
-            SetTabWidth(txtInput);
-        }
-
-        public void SetTabWidth(Control ctl)
-        {
-            System.Drawing.Graphics graphics = ctl.CreateGraphics();
-            SendMessage(ctl.Handle, EM_SETTABSTOPS, 8,
-                new int[] { 38 * 4, 46 * 4, 58 * 4, 66 * 4, 74 * 4, 82 * 4, 90 * 4, 112 * 4, 132 * 4 });
         }
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
-            List<NewProductData> productDataToSave;
-            List<ProductBrand> brands;
-            if (ParseInput(out productDataToSave, out brands))
-                return;
-            foreach (ProductBrand brand in brands)
+            foreach (NewProductData rec in mProductDataToSave)
+            {
+                using (Ambient.DbSession.Activate())
+                {
+                    List<VendorProduct> partNumMatches = OrderingRepositories.VendorProduct.Get(
+                        mVendor.Id, rec.VendorProduct.VendorPartNum);
+                    if (partNumMatches.Count > 0)
+                    {
+                        MessageBox.Show(string.Format("Vendor code {0} already exists in database.",
+                            rec.VendorProduct.VendorPartNum));
+                        return;
+                    }
+                    if (!rec.Product.ProductBrandId.IsNull)
+                    {
+                        List<Product> brandNameSizeMatches = OrderingRepositories.Product.Get(
+                            rec.Product.ProductBrandId, rec.Product.ProductName, rec.Product.Size);
+                        if (brandNameSizeMatches.Count > 0)
+                        {
+                            MessageBox.Show(string.Format(
+                                "Product name \"{0}\", brand \"{1}\" and size \"{2}\" already exists in database.",
+                                rec.Product.ProductName, rec.BrandName, rec.Product.Size));
+                            return;
+                        }
+                    }
+                }
+                foreach (NewProductData otherRecord in mProductDataToSave)
+                {
+                    if (otherRecord != rec)
+                    {
+                        if (otherRecord.VendorProduct.VendorPartNum == rec.VendorProduct.VendorPartNum)
+                        {
+                            MessageBox.Show(string.Format("Vendor code {0} exists multiple times in input.",
+                                rec.VendorProduct.VendorPartNum));
+                            return;
+                        }
+                        if (otherRecord.Product.ProductName == rec.Product.ProductName)
+                        {
+                            if (otherRecord.BrandName == rec.BrandName)
+                            {
+                                if (otherRecord.Product.Size == rec.Product.Size)
+                                {
+                                    MessageBox.Show(string.Format(
+                                        "Product name \"{0}\", brand \"{1}\" and size \"{2}\" exists multiple times in input.",
+                                        rec.Product.ProductName, rec.BrandName, rec.Product.Size));
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (ProductBrand brand in mBrands)
             {
                 if (!brand.IsPersisted)
                 {
@@ -69,7 +101,7 @@ namespace Willowsoft.Ordering.UI.SetupForms
                 }
             }
             int recordsCreated = 0;
-            foreach (NewProductData objToSave in productDataToSave)
+            foreach (NewProductData objToSave in mProductDataToSave)
             {
                 using (ITranScope tranScope = Ambient.DbSession.CreateTranScope())
                 {
@@ -88,158 +120,115 @@ namespace Willowsoft.Ordering.UI.SetupForms
             MessageBox.Show(string.Format("{0} products created", recordsCreated));
         }
 
-        private bool ParseInput(out List<NewProductData> productDataToSave,
-            out List<ProductBrand> brands)
+        private bool ParseInput()
         {
-            StringReader reader = new StringReader(txtInput.Text);
-            productDataToSave = new List<NewProductData>();
+            
+            StringReader reader = new StringReader(System.Windows.Forms.Clipboard.GetText());
+            mProductDataToSave = new List<NewProductData>();
             using (Ambient.DbSession.Activate())
             {
-                brands = OrderingRepositories.ProductBrand.GetAll();
+                mBrands = OrderingRepositories.ProductBrand.GetAll();
             }
             int lineNumber = 0;
             for (; ; )
             {
                 string line = reader.ReadLine();
-                if (line == null)
+                if (string.IsNullOrEmpty(line))
                     break;
                 lineNumber++;
                 string[] fields = line.Split('\t');
-                if (fields.Length != 10)
-                {
-                    MessageBox.Show(string.Format("Wrong number of fields on line {0}", lineNumber));
+                if (TryProcessFields(fields, lineNumber))
                     return true;
-                }
-                if (ValidateString("Product name", fields[0], 4, 100, lineNumber))
-                    return true;
-                if (ValidateString("Product size", fields[1], 0, 30, lineNumber))
-                    return true;
-                if (ValidateString("Vendor code", fields[2], 1, 30, lineNumber))
-                    return true;
-                decimal retailPrice;
-                if (ValidateDecimal("Retail price", fields[3], out retailPrice, lineNumber))
-                    return true;
-                decimal caseCost;
-                if (ValidateDecimal("Case cost", fields[4], out caseCost, lineNumber))
-                    return true;
-                int countInCase;
-                if (ValidateInt("Count in case", fields[5], out countInCase, lineNumber))
-                    return true;
-                decimal eachCost;
-                if (ValidateDecimal("Each cost", fields[6], out eachCost, lineNumber))
-                    return true;
-                string brandName = fields[7];
-                if (ValidateString("Brand name", brandName, 3, 80, lineNumber))
-                    return true;
-                ProductBrand brandToUse = null;
-                foreach (ProductBrand existingBrand in brands)
-                {
-                    if (NormalizeBrandName(existingBrand.BrandName).Equals(
-                        NormalizeBrandName(brandName), StringComparison.OrdinalIgnoreCase))
-                    {
-                        brandToUse = existingBrand;
-                        break;
-                    }
-                }
-                if (brandToUse == null)
-                {
-                    DialogResult newBrandAnswer = MessageBox.Show(string.Format(
-                        "Brand name \"{0}\" not found. Do you want to create it?", brandName),
-                        "New Brand Name", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                    if (newBrandAnswer != DialogResult.Yes)
-                        return true;
-                    brandToUse = new ProductBrand(new ProductBrandId(), brandName, string.Empty, true, string.Empty,
-                        DateTime.Now, DateTime.Now);
-                    brands.Add(brandToUse);
-                }
-                ProductSubCategory subCatToUse = null;
-                string subCatName = fields[8];
-                foreach (ProductSubCategory subCat in mAllSubcategories)
-                {
-                    if (subCat.SubCategoryName.Equals(subCatName, StringComparison.OrdinalIgnoreCase))
-                    {
-                        subCatToUse = subCat;
-                        break;
-                    }
-                }
-                if (subCatToUse == null)
-                {
-                    MessageBox.Show(string.Format("Subcategory \"{0}\" not found.", subCatName),
-                        "No such subcategory", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return true;
-                }
-                bool isActive;
-                if (fields[9].ToUpper() == "Y")
-                    isActive = true;
-                else if (fields[9].ToUpper() == "N")
-                    isActive = false;
-                else
-                {
-                    MessageBox.Show(string.Format("Invalid active flag on line {0}", lineNumber));
-                    return true;
-                }
-                NewProductData rec = new NewProductData();
-                rec.Product = new Product(new ProductId(), fields[0], subCatToUse.Id, fields[1],
-                    retailPrice, brandToUse.Id, string.Empty, string.Empty, isActive,
-                    false, false, false, false, 0, 0, 0, 0, string.Empty, 0.0m, 0.0m, DateTime.Now, DateTime.Now);
-                rec.VendorProduct = new VendorProduct(new VendorProductId(), mVendor.Id, new ProductId(),
-                    0m, fields[2], caseCost, countInCase, eachCost, isActive, isActive,
-                    false, false, new DateTime(1980, 1, 1), string.Empty, false, false, string.Empty,
-                    DateTime.Now, DateTime.Now);
-                rec.BrandName = brandName;
-                productDataToSave.Add(rec);
             }
-            foreach (NewProductData rec in productDataToSave)
+            return false;
+        }
+
+        private bool TryProcessFields(string[] fields, int lineNumber)
+        {
+            if (fields.Length != 10)
             {
-                using (Ambient.DbSession.Activate())
+                MessageBox.Show(string.Format("Wrong number of fields on line {0}", lineNumber));
+                return true;
+            }
+            if (ValidateString("Product name", fields[0], 4, 100, lineNumber))
+                return true;
+            if (ValidateString("Product size", fields[1], 0, 30, lineNumber))
+                return true;
+            if (ValidateString("Vendor code", fields[2], 1, 30, lineNumber))
+                return true;
+            decimal retailPrice;
+            if (ValidateDecimal("Retail price", fields[3], out retailPrice, lineNumber))
+                return true;
+            decimal caseCost;
+            if (ValidateDecimal("Case cost", fields[4], out caseCost, lineNumber))
+                return true;
+            int countInCase;
+            if (ValidateInt("Count in case", fields[5], out countInCase, lineNumber))
+                return true;
+            decimal eachCost;
+            if (ValidateDecimal("Each cost", fields[6], out eachCost, lineNumber))
+                return true;
+            string brandName = fields[7];
+            if (ValidateString("Brand name", brandName, 3, 80, lineNumber))
+                return true;
+            ProductBrand brandToUse = null;
+            foreach (ProductBrand existingBrand in mBrands)
+            {
+                if (NormalizeBrandName(existingBrand.BrandName).Equals(
+                    NormalizeBrandName(brandName), StringComparison.OrdinalIgnoreCase))
                 {
-                    List<VendorProduct> partNumMatches = OrderingRepositories.VendorProduct.Get(
-                        mVendor.Id, rec.VendorProduct.VendorPartNum);
-                    if (partNumMatches.Count > 0)
-                    {
-                        MessageBox.Show(string.Format("Vendor code {0} already exists in database.",
-                            rec.VendorProduct.VendorPartNum));
-                        return true;
-                    }
-                    if (!rec.Product.ProductBrandId.IsNull)
-                    {
-                        List<Product> brandNameSizeMatches = OrderingRepositories.Product.Get(
-                            rec.Product.ProductBrandId, rec.Product.ProductName, rec.Product.Size);
-                        if (brandNameSizeMatches.Count > 0)
-                        {
-                            MessageBox.Show(string.Format(
-                                "Product name \"{0}\", brand \"{1}\" and size \"{2}\" already exists in database.",
-                                rec.Product.ProductName, rec.BrandName, rec.Product.Size));
-                            return true;
-                        }
-                    }
-                }
-                foreach (NewProductData otherRecord in productDataToSave)
-                {
-                    if (otherRecord != rec)
-                    {
-                        if (otherRecord.VendorProduct.VendorPartNum == rec.VendorProduct.VendorPartNum)
-                        {
-                            MessageBox.Show(string.Format("Vendor code {0} exists multiple times in input.",
-                                rec.VendorProduct.VendorPartNum));
-                            return true;
-                        }
-                        if (otherRecord.Product.ProductName == rec.Product.ProductName)
-                        {
-                            if (otherRecord.BrandName == rec.BrandName)
-                            {
-                                if (otherRecord.Product.Size == rec.Product.Size)
-                                {
-                                    MessageBox.Show(string.Format(
-                                        "Product name \"{0}\", brand \"{1}\" and size \"{2}\" exists multiple times in input.",
-                                        rec.Product.ProductName, rec.BrandName, rec.Product.Size));
-                                    return true;
-                                }
-                            }
-                        }
-                    }
+                    brandToUse = existingBrand;
+                    break;
                 }
             }
+            if (brandToUse == null)
+            {
+                DialogResult newBrandAnswer = MessageBox.Show(string.Format(
+                    "Brand name \"{0}\" not found. Do you want to create it?", brandName),
+                    "New Brand Name", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (newBrandAnswer != DialogResult.Yes)
+                    return true;
+                brandToUse = new ProductBrand(new ProductBrandId(), brandName, string.Empty, true, string.Empty,
+                    DateTime.Now, DateTime.Now);
+                mBrands.Add(brandToUse);
+            }
+            ProductSubCategory subCatToUse = null;
+            string subCatName = fields[8];
+            foreach (ProductSubCategory subCat in mAllSubcategories)
+            {
+                if (subCat.SubCategoryName.Equals(subCatName, StringComparison.OrdinalIgnoreCase))
+                {
+                    subCatToUse = subCat;
+                    break;
+                }
+            }
+            if (subCatToUse == null)
+            {
+                MessageBox.Show(string.Format("Subcategory \"{0}\" not found.", subCatName),
+                    "No such subcategory", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
+            }
+            bool isActive;
+            if (fields[9].ToUpper() == "Y")
+                isActive = true;
+            else if (fields[9].ToUpper() == "N")
+                isActive = false;
+            else
+            {
+                MessageBox.Show(string.Format("Invalid active flag on line {0}", lineNumber));
+                return true;
+            }
+            NewProductData rec = new NewProductData();
+            rec.Product = new Product(new ProductId(), fields[0], subCatToUse.Id, fields[1],
+                retailPrice, brandToUse.Id, string.Empty, string.Empty, isActive,
+                false, false, false, false, 0, 0, 0, 0, string.Empty, 0.0m, 0.0m, DateTime.Now, DateTime.Now);
+            rec.VendorProduct = new VendorProduct(new VendorProductId(), mVendor.Id, new ProductId(),
+                0m, fields[2], caseCost, countInCase, eachCost, isActive, isActive,
+                false, false, new DateTime(1980, 1, 1), string.Empty, false, false, string.Empty,
+                DateTime.Now, DateTime.Now);
+            rec.BrandName = brandName;
+            mProductDataToSave.Add(rec);
+            ShowNewProductData(rec);
             return false;
         }
 
@@ -286,8 +275,25 @@ namespace Willowsoft.Ordering.UI.SetupForms
             string importLine = enterForm.GetImportLine(mVendor.Id, mAllSubcategories);
             if (!string.IsNullOrEmpty(importLine))
             {
-                txtInput.Text = txtInput.Text + importLine + Environment.NewLine;
+                TryProcessFields(importLine.Split('\t'), 1);
             }
+        }
+
+        private void ShowNewProductData(NewProductData newProd)
+        {
+            ListViewItem item = new ListViewItem(new string[] {
+                newProd.Product.ProductName,
+                newProd.Product.Size,
+                newProd.VendorProduct.VendorPartNum,
+                newProd.Product.RetailPrice.ToString("C2"),
+                newProd.VendorProduct.CaseCost.ToString("C2"),
+                newProd.VendorProduct.CountInCase.ToString(),
+                newProd.VendorProduct.EachCost.ToString("C2"),
+                newProd.BrandName,
+                mAllSubcategories.Find(subcat=>newProd.Product.ProductSubCategoryId==subcat.Id).SubCategoryName,
+                newProd.Product.IsActive.ToString()
+                });
+            lvwNewProducts.Items.Add(item);
         }
     }
 
